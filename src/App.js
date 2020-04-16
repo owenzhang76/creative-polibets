f5691b41c69fc834767e8900f1ef871b0e0b2a8b
Owen Zhang <owenzhang76@gmail.com>
2020-04-16 17:26:27 -0600

    working through react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import homePage from "./components/homePage.component";
+// import signupPage from "./components/signup-page.component";
+// import createPostPage from "./components/create-post-page.component";
 
 function App() {
   return (
-    <div className="body-container">
-      <h1 className="header">Hello</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+
+      <Route path="/" exact component={homePage}></Route>
+      {/* <Route path="/login" exact component={loginPage}></Route>
+      <Route path="/signup" exact component={signupPage}></Route>
+      <Route path="/createPost" exact component={createPostPage}></Route> */}
+    </Router>
   );
 }
 
