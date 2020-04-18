cfc3be6bf0e0035427b0342e21f15583ba86030a
Owen Zhang <owenzhang76@gmail.com>
2020-04-17 21:40:05 -0600

    working on homepage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
 import registerForm from "./components/registerForm.component";
+import homepage from './components/homepage.component';
 // import signupPage from "./components/signup-page.component";
 // import createPostPage from "./components/create-post-page.component";
 
@@ -15,6 +16,7 @@ function App() {
       <br />
 
       <Route path="/" exact component={registerForm}></Route>
+      <Route path="/home" exact component={homepage}></Route>
       {/* <Route path="/login" exact component={loginPage}></Route>
       <Route path="/signup" exact component={signupPage}></Route>
       <Route path="/createPost" exact component={createPostPage}></Route> */}
---
+++ src/components/homepage.component.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class homepage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            email: '',
+            firstname: '',
+            lastname: '',
+        };
+    };
+
+    componentDidMount() {
+        this.setState({
+            
+        });
+    }
+
+}
